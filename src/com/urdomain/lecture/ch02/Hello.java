622a37d9f59558129263c715f51e6a8829583ee7
2milyjun9 <96867852+2milyjun9@users.noreply.github.com>
2022-02-04 15:56:42 +0900

    0

--- src/com/urdomain/lecture/ch02/Hello.java
+++ src/com/urdomain/lecture/ch02/Hello.java
@@ -6,6 +6,16 @@ public class Hello {
 		// TODO Auto-generated method stub
 
 		System.out.println("졸려용");
+		
+		
+		int a;
+		a =1;
+		
+		int b=2;
+		int c=0;
+		c=a+b;
+		
+		System.out.println(c);
 	}
 
 }
---
+++ src/com/urdomain/lecture/ch02/Variable.java
@@ -0,0 +1,80 @@
+package com.urdomain.lecture.ch02;
+
+public class Variable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String fullname = "Mirim Jung";
+	    String name = "Mirim";
+		
+		
+		int roomNumber = 1;
+		int roomnumber = 2;
+		
+		System.out.println("roomNumber:" + roomNumber);
+		System.out.println("roomnumber:" + roomnumber);
+		
+	 
+		
+		
+		int a; //선언 
+		a=1; //대입초기화
+		int b = 1; //선언과초기화
+		int c = 0;
+		
+		 c = a + b ;
+		
+		boolean defaultNy = false;
+		byte aa = 0;
+		short bb = 0;
+		int cc =0;
+		long dd = 0;
+		
+		float ee = 0.0f;
+		double ff = 0.0;
+		
+		char gg = ' ';
+		
+		String hh= "";
+		
+		
+		int bbb = 1;
+		System.out.println(bbb);
+		System.out.println("Mirim");
+		System.out.println("Mirim:" + bbb + "Genius!!");
+		
+		System.out.println("한칸들여쓰기");
+		System.out.println("\t 한칸들여쓰기");
+		
+		System.out.println("한줄바꿈1");
+		System.out.println("\n 한줄바꿈");
+
+		System.out.println("한줄바꿈2");
+		System.out.println("\r한줄바꿈");
+		
+		System.out.println("\'나는생각한다\'");
+		System.out.println("쏘크라테스가 말했다 \"나는존재한다.\"");
+		System.out.println("\\");
+		
+		System.out.print("asdfasdf");
+		System.out.print("asdfasdf");
+		System.out.print("\nadfasdf");
+		System.out.print("\radfasdf");
+		System.out.print("\r\nadfasdf");
+		System.out.print("\n\radfasdf");
+
+		
+		if(a==1) {
+			int aaaa = 10;
+		int bbbb = aaaa+a;
+		System.out.println("bbbb:"+bbbb);
+		} else {
+			
+		}
+	
+	
+
+	}
+}
+
