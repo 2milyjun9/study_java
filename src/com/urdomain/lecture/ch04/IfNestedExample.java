535ba334ba6e69f9c60cb8bca035dd6933656c8f
2milyjun9 <96867852+2milyjun9@users.noreply.github.com>
2022-02-10 17:55:46 +0900

    0

--- src/com/urdomain/lecture/ch04/IfNestedExample.java
+++ src/com/urdomain/lecture/ch04/IfNestedExample.java
@@ -4,36 +4,43 @@ public class IfNestedExample {
 
 	public static void main(String[] args) {
 
-		
-		int score = 75;
-		String grade = "";
-		
-		if(score >=90) {
-		   if(score >=95) {
-			   grade = "A+";
-		   }else if (score>=85) {
-				   grade = "B+";
-			   }
-			   else if (score>=80) {
-				   grade = "B";
-			   }
-			   else if (score>=75) {
-				   grade = "C+";
-			   }
-			   else if (score>=70) {
-				   grade = "C";
-			   }
-			   else if (score>=75) {
-				   grade = "D+";
-			   }
-			   else if (score>=70) {
-				   grade = "D";
-			   }
-		   }else {
-			   grade = "F 정신차리세요";
-			  
-		}
-		System.out.println("학점은:"+ grade);
-	}
+		/*
+		 * int score = 75; String grade = "";
+		 * 
+		 * if (score >= 90) { if (score >= 95) { System.out.println("A+"); } else {
+		 * System.out.println("A"); } } else if (score >= 80) { if (score >= 85) {
+		 * System.out.println("B+"); } else { System.out.println("B"); } } else if
+		 * (score >= 70) { if (score >= 75) { System.out.println("C+"); } else {
+		 * System.out.println("C"); } } else { System.out.println("F"); } } }
+		 */
 
+int score = (int) 4.0;
+String grade = "";
+
+if (score >= 4.0) {
+	if (score >= 4.3) {
+		grade = "A+";
+	} else {
+		grade = "A";
+	}
+}
+else if  (score >= 3.0) {
+		if (score >= 3.5) {
+			grade = "B+";
+		} else {
+			grade = "B";
+		}
+	}
+else if (score >= 2.0) {
+			if (score >= 2.5) {
+				grade = "C+";
+			} else {
+				grade = "C";
+			}
+		}
+else  {
+	grade = "F";
+			}
+System.out.printf("학점은:" + grade);
+		}
 }
