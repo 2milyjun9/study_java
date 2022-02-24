c6099b4359a76ed217294ff157d6abe30083ef8c
2milyjun9 <96867852+2milyjun9@users.noreply.github.com>
2022-02-24 19:36:49 +0900

    0

---
+++ src/com/urdomain/lecture/ch07/Banana.java
@@ -0,0 +1,20 @@
+package com.urdomain.lecture.ch07;
+
+public class Banana extends Apple{
+
+	
+	private String strBanana = " strBanana";
+	
+	public void printApple() {
+		System.out.println("Banana");
+	}
+	
+	public String getStrBanana() {
+		return strBanana;
+	}
+	
+	public void setStrBanana(String strBanana) {
+		this.strBanana = strBanana;
+	}
+	
+	}
---
+++ src/com/urdomain/lecture/ch07/Durian.java
@@ -0,0 +1,20 @@
+package com.urdomain.lecture.ch07;
+
+public class Durian extends Cherry{
+
+	
+	private String strDurian = " strDurian";
+	
+	public void printDurian() {
+		System.out.println("Durian");
+	}
+	
+	public String getstrDurian() {
+		return strDurian;
+	}
+	
+	public void setStrDurian(String strDurian) {
+		this.strDurian = strDurian;
+	}
+	
+	}
